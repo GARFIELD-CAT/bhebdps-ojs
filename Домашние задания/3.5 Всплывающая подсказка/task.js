3f40b08f19cd36a8f2aa0eb6153d236d8364b5fb
Yagunov Denis <spb.jagunov.denis@gmail.com>
2024-12-04 11:45:32 +0300

    Реализовано задание Всплывающая подсказка

--- Домашние задания/3.5 Всплывающая подсказка/task.js
+++ Домашние задания/3.5 Всплывающая подсказка/task.js
@@ -1,17 +1,29 @@
-document.querySelectorAll(".menu__link").forEach((link) => {
+document.querySelectorAll(".has-tooltip").forEach((link) => {
   link.addEventListener("click", function (event) {
-    let currentMenu = this.nextElementSibling;
+    event.preventDefault();
+    let currentLink = event.target;
+    let currentTooltip = this.nextElementSibling;
+    let coordinates = currentLink.getBoundingClientRect();
 
-    if (currentMenu && currentMenu.classList.contains("menu_sub")) {
-      event.preventDefault();
-
-      document.querySelectorAll(".menu_active").forEach((activeMenu) => {
-        if (activeMenu !== currentMenu) {
-          activeMenu.classList.remove("menu_active");
-        }
-      });
-
-      currentMenu.classList.add("menu_active");
+    if (!currentTooltip || !currentTooltip.classList.contains("tooltip")) {
+      let tooltipDiv = document.createElement("div");
+      tooltipDiv.className = "tooltip";
+      tooltipDiv.innerHTML = currentLink.title;
+      tooltipDiv.style.left = `${coordinates.left}px`;
+      tooltipDiv.style.top = `${coordinates.top + 20}px`;
+      link.after(tooltipDiv);
+      currentTooltip = tooltipDiv;
+    } else {
+      currentTooltip.style.left = `${coordinates.left}px`;
+      currentTooltip.style.top = `${coordinates.top + 20}px`;
     }
+
+    document.querySelectorAll(".tooltip_active").forEach((activeTooltip) => {
+      if (activeTooltip !== currentTooltip) {
+        activeTooltip.classList.remove("tooltip_active");
+      }
+    });
+
+    currentTooltip.classList.toggle("tooltip_active");
   });
 });
